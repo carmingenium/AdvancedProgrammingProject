9248c8908f306f8f9539fe0be885f6c8d1c46b40
aerasis-com <ekremsever@gmail.com>
2024-05-14 16:55:34 +0300

    OnScreenBall

---
+++ app/src/main/java/com/example/advancedprogrammingproject/OnScreenBall.java
@@ -0,0 +1,27 @@
+package com.example.advancedprogrammingproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OnScreenBall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_on_screen_ball);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ObjectAnimator animation = ObjectAnimator.ofFloat(view, "translationX", 100f);
+        animation.setDuration(2000);
+        animation.start();
+    }
+}
