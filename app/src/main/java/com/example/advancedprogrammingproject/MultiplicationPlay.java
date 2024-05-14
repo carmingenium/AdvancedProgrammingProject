7a9e0238aff721b71d75308a5b59dd5a5b1d3fec
aerasis-com <ekremsever@gmail.com>
2024-05-14 19:30:34 +0300

    Multiplication

--- app/src/main/java/com/example/advancedprogrammingproject/MultiplicationPlay.java
+++ app/src/main/java/com/example/advancedprogrammingproject/MultiplicationPlay.java
@@ -1,6 +1,12 @@
 package com.example.advancedprogrammingproject;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +25,131 @@ public class MultiplicationPlay extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        ConfigureQ1Input();
+        ConfigureQ2Input();
+        ConfigureQ3Input();
+        ConfigureQ4Input();
+        ConfigureQ5Input();
+    }
+    public void ConfigureQ1Input() {
+
+        EditText answer1 = (EditText) findViewById(R.id.question1);
+        answer1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean b) {
+                int ans1 ;
+                String val1 = answer1.getText().toString();
+                if(!val1.isEmpty()) {
+                    try {
+                        ans1 = Integer.parseInt(answer1.getText().toString());
+                        if (ans1 == 5) {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Success), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Failed), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    catch (NumberFormatException excl) {
+                        Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_TextOnly), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+    public void ConfigureQ2Input() {
+
+        EditText answer = (EditText) findViewById(R.id.question2);
+        answer.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean b) {
+                int ans ;
+                String val = answer.getText().toString();
+                if(!val.isEmpty()) {
+                    try {
+                        ans = Integer.parseInt(answer.getText().toString());
+                        if (ans == 42) {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Success), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Failed), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    catch (NumberFormatException excl) {
+                        Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_TextOnly), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+    public void ConfigureQ3Input() {
+
+        EditText answer = (EditText) findViewById(R.id.question3);
+        answer.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean b) {
+                int ans ;
+                String val = answer.getText().toString();
+                if(!val.isEmpty()) {
+                    try {
+                        ans = Integer.parseInt(answer.getText().toString());
+                        if (ans == 20) {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Success), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Failed), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    catch (NumberFormatException excl) {
+                        Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_TextOnly), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+    public void ConfigureQ4Input() {
+
+        EditText answer = (EditText) findViewById(R.id.question4);
+        answer.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean b) {
+                int ans ;
+                String val = answer.getText().toString();
+                if(!val.isEmpty()) {
+                    try {
+                        ans = Integer.parseInt(answer.getText().toString());
+                        if (ans == 14) {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Success), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Failed), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    catch (NumberFormatException excl) {
+                        Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_TextOnly), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+    public void ConfigureQ5Input() {
+
+        EditText answer = (EditText) findViewById(R.id.question5);
+        answer.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean b) {
+                int ans ;
+                String val = answer.getText().toString();
+                if(!val.isEmpty()) {
+                    try {
+                        ans = Integer.parseInt(answer.getText().toString());
+                        if (ans == 77) {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Success), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_Failed), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    catch (NumberFormatException excl) {
+                        Toast.makeText(MultiplicationPlay.this, getString(R.string.Warn_TextOnly), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
         });
     }
 }
