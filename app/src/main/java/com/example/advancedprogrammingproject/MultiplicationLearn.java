e32a6f833a81e693cf8d9582425f89da8f975c9f
Dağhan Sever <daghansever@gmail.com>
2024-05-14 15:10:23 +0300

    multiplication learn done

--- app/src/main/java/com/example/advancedprogrammingproject/MultiplicationLearn.java
+++ app/src/main/java/com/example/advancedprogrammingproject/MultiplicationLearn.java
@@ -1,6 +1,9 @@
 package com.example.advancedprogrammingproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,26 @@ public class MultiplicationLearn extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        configurePlayButton();
+        configureQuitButton();
+    }
+    public void configureQuitButton(){
+        Button quitButton = (Button) findViewById(R.id.multiplication_learn_quit);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    public void configurePlayButton(){
+        Button playButton = (Button) findViewById(R.id.multiplication_play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MultiplicationLearn.this, MultiplicationPlay.class));
+            }
         });
     }
 }
