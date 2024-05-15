352b5ae209e70acb74f22efefbde333576b50c66
aerasis-com <ekremsever@gmail.com>
2024-05-15 08:21:25 +0300

    NumberCounting

--- app/src/main/java/com/example/advancedprogrammingproject/NumberCountingLearn.java
+++ app/src/main/java/com/example/advancedprogrammingproject/NumberCountingLearn.java
@@ -1,6 +1,9 @@
 package com.example.advancedprogrammingproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,26 @@ public class NumberCountingLearn extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        ConfigureQuitButton();
+        ConfigurePlayButton();
+    }
+    public void ConfigurePlayButton() {
+        Button playButton = (Button) findViewById(R.id.numbercount_play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NumberCountingLearn.this, NumberCountingPlay.class));
+            }
+        });
+    }
+    public void ConfigureQuitButton() {
+        Button playButton = (Button) findViewById(R.id.numbercount_quit);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
